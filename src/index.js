ef08edf98e0314d4fd711e8ef746513f9b974fea
Guilherme Rocha <guirocha.hopeisaba@gmail.com>
2021-03-10 18:03:54 -0300

    can send messages and your location to all users

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const Filter = require("bad-words");
+const { generateMessage } = require("./utils/messages");
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.POR || 3333;
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectoryPath));
+
+// let count = 0;
+
+io.on("connection", (socket) => {
+  console.log("New WebSocket connection");
+
+  socket.emit("message", generateMessage("Welcome!"));
+
+  socket.broadcast.emit("message", generateMessage("A new user has joined!"));
+
+  socket.on("sendMessage", (message, callback) => {
+    const filter = new Filter();
+    if (filter.isProfane(message)) {
+      return callback("Profanity is not allowed");
+    }
+    io.emit("message", generateMessage(message));
+    callback("cool");
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", generateMessage("A user has left"));
+  });
+
+  socket.on("sendLocation", (coords, callback) => {
+    io.emit(
+      "locationMessage",
+      `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+    );
+    callback();
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}...`);
+});
