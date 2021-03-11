bddfea11809f5f4c9979ba3b46623954658e16ce
Guilherme Rocha <guirocha.hopeisaba@gmail.com>
2021-03-11 12:51:52 -0300

    made text chat input a little better in mobile

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirectoryPath));
 
 io.on("connection", (socket) => {
-  console.log("New WebSocket connection");
+  // console.log("New WebSocket connection");
 
   socket.on("join", (options, callback) => {
     const { error, user } = addUser({ id: socket.id, ...options });
@@ -32,15 +32,16 @@ io.on("connection", (socket) => {
       return callback(error);
     }
 
-    //É usado para se juntar a uma nova sala e poder enviar msgs especificamente para ela
+    // É usado para se juntar a uma nova sala e poder enviar msg
+    // especificamente para essa sala
     socket.join(user.room);
 
-    socket.emit("message", generateMessage("Admin", "Welcome!"));
+    socket.emit("message", generateMessage("admin", "Bem vindo!"));
     socket.broadcast
       .to(user.room)
       .emit(
         "message",
-        generateMessage("Admin", `${user.username} has joined!`)
+        generateMessage("admin", `${user.username} se juntou a sala de chat!`)
       );
     io.to(user.room).emit("roomData", {
       room: user.room,
@@ -53,10 +54,13 @@ io.on("connection", (socket) => {
   socket.on("sendMessage", (message, callback) => {
     const user = getUser(socket.id);
 
-    const filter = new Filter();
-    if (filter.isProfane(message)) {
-      return callback("Profanity is not allowed");
-    }
+    // É usado para filtrar palavras consideradas de
+    // baixo calão
+    // const filter = new Filter();
+    // if (filter.isProfane(message)) {
+    //   return callback("Profanity is not allowed");
+    // }
+
     io.to(user.room).emit("message", generateMessage(user.username, message));
     callback("");
   });
@@ -80,7 +84,7 @@ io.on("connection", (socket) => {
     if (user) {
       io.to(user.room).emit(
         "message",
-        generateMessage("Admin", `${user.username} has left!`)
+        generateMessage("admin", `${user.username} has left!`)
       );
       io.to(user.room).emit("roomData", {
         room: user.room,
@@ -92,5 +96,5 @@ io.on("connection", (socket) => {
 });
 
 server.listen(port, () => {
-  console.log(`Server is up on port ${port}...`);
+  console.log(`Servidor está rodando na porta ${port}...`);
 });
